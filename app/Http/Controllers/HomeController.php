6b27ea8ee518932a21231df09db4701b8ddc4b5d
ezegyfa2 <darvasvok@gmail.com>
2023-09-07 16:41:45 +0200

    Fixing controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,13 +12,6 @@ class HomeController extends Controller
         $templateParams = new stdClass;
         $templateParams->language = strtoupper(App::currentLocale());
         $templateParams->current_url = "/home";
-        return DynamicTemplateMethods::getTemplateDynamicPage('electrician_index', $templateParams, 'app');
-    }
-
-    public function products() {
-        $templateParams = new stdClass;
-        $templateParams->language = "HU";
-        $templateParams->current_url = "/products";
-        return DynamicTemplateMethods::getTemplateDynamicPage('electrician_products', $templateParams, 'app');
+        return DynamicTemplateMethods::getTemplateDynamicPage('electrician_welcome', $templateParams, 'app');
     }
 }
