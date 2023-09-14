8441b944b1b14882b11b65a50ac31de535c99067
József Bagota <jozsef.bagota@gmail.com>
2023-09-14 17:08:05 +0200

    new language

---
+++ resources/lang/en/layout.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'navbar' => [
+        'title' => 'Electrician',
+        'about_us' => 'About us',
+        'our_products' => 'Products',
+        'button_title' => 'Appointment'
+    ],
+    'footer' => [
+        'columns' => [
+            'links1' => [
+                'about_us' => 'About us',
+                'privacy_policy' => 'Privacy policy',
+                'contacts' => 'Contacts'
+            ],
+            'links2' => [
+                'our_products' => 'Products',
+                'services' => 'Services'
+            ],
+            'links3' => [
+                'for_companies' => 'For companies',
+                'for_customers' => 'For customers'
+            ],
+        ],
+        'address' => [
+            'street' => '399 Crowfield Road',
+            'city' => 'Phoenix, Arizona 85012',
+            'email' => 'asff@fdsfsdc.com',
+            'open' => 'Monday-Friday 8:00-17:00',
+            'phone_number' => '+602-926-5809'
+        ],
+        'copyright' => [
+            'copy' => 'Electrician @ All Rights Reserved 2023',
+        ],
+    ],
+];
