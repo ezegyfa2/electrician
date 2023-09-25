9d00307633f8328cad4d0ff45521e1f65f4a6ec9
ezegyfa2 <darvasvok@gmail.com>
2023-09-25 10:41:26 +0300

    Changing

--- resources/lang/hu/routes.php
+++ resources/lang/hu/routes.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'asd' => 'asd_hu',
+
 ];
---
+++ resources/lang/hu/welcome.php
@@ -0,0 +1,73 @@
+<?php
+
+return [
+    'main_description' => [
+        'title' => 'Bartos György',
+        'content' => 'Villanyszerelőként már évek óta szolgálom Csíkszereda lakóinak elektronikus szükségleteit. Az elkötelezett csapatunk mindig készen áll arra, hogy segítsen az elektromos rendszerek tervezésében, telepítésében és karbantartásában. Legyen szó egy egyszerű kis javításról vagy egy teljes villamosrendszerek tervezéséről és kiépítéséről, itt vagyunk, hogy Önnek segítsünk.',
+        'button_content' => 'időpontkérés',
+    ],
+    'carousel' => [
+        'slide1' => [
+            'title' => 'Villanyszerelés',
+            'content' => 'Megbízható és biztonságos megoldások!',
+        ],
+        'slide2' => [
+            'title' => 'Napelemek',
+            'content' => 'Hatékony és fenntartható napelemes rendszerek!',
+        ],
+        'slide3' => [
+            'title' => 'Automatizálás',
+            'content' => 'Okos megoldások a mindennapokban!',
+        ],
+    ],
+    'skew_carousel' => [
+        'sections' => [
+            'slide1' => [
+                'title' => 'Okos megoldások',
+                'content' => 'Weboldalunkon okos megoldásokat kínálunk az elektromos szerelés területén, hogy növeljük otthona energiahatékonyságát és biztonságát.',
+            ],
+            'slide2' => [
+                'title' => 'Villanyszerelés',
+                'content' => 'Gyors és precíz munkavégzés legyen szó lakóházról, irodáról\nvagy ipari komplexumról.\nRugalmasan alkalmazkodunk az Ön igényeihez.',
+            ],
+            'slide3' => [
+                'title' => 'Napelemek telepítése',
+                'content' => 'Napelemek szerelése, telepítése rövid határidővel! Precíz munkavégzés az igényeknek megfelelően.',
+            ],
+        ],
+    ],
+    'description' => [
+        'content1' => [
+            'title' => 'Napelemek szerelése, telepítése',
+            'content' => 'Egy napenergiás rendszer telepítése során először a napelemek elhelyezését és teljesítményét tervezzük meg. Ezt követően összeállítjuk a szükséges dokumentációt, beleértve az engedélykéréseket is. A rendszer telepítése magában foglalja a napelemek rögzítését, az inverterek és panelösszekötések telepítését. Miután a rendszer műszakilag elkészült, beállítjuk az invertereket és teszteljük a teljesítményt. Végül hivatalosan bejelentjük a rendszer készültségét a hatóságoknak, amely után az üzembe helyezés megtörténik.',
+            'button_content' => 'időpontkérés',
+        ],
+        'content2' => [
+            'title' => 'Automatizálás',
+            'content' => 'Az elektromos automatizálás során folyamatok és rendszerek irányítása automatikusan történik elektromos eszközök segítségével. Ennek köszönhetően a műveletek előre meghatározott módon zajlanak, emberi beavatkozás nélkül. Modern rendszerek szenzorokkal figyelik a folyamatokat, vezérlőegységek irányítják azokat, míg aktuátorok fizikai cselekvéseket hajtanak végre. Az ipari területeken, mint például az autógyártás vagy gyártósorok, az elektromos automatizálás hatékonyabbá teszi a folyamatokat és minimalizálja a hibalehetőségeket.',
+            'button_content' => 'időpontkérés',
+        ],
+        'content3' => [
+            'title' => 'Villanyszerelés',
+            'content' => 'Professzionális villanyszerelési szolgáltatásokat kínálunk ügyfeleinknek. Tapasztalt csapatunk vállalja épületek és létesítmények teljeskörű elektromos rendszereinek tervezését, telepítését és karbantartását. Legyen szó lakóházról, irodáról vagy ipari komplexumról, rugalmasan alkalmazkodunk az Ön igényeihez. Biztonság és minőség áll szolgáltatásaink középpontjában, melyre garancia a szakértelemmel és legújabb technológiákkal felszerelt csapatunk. Bízza ránk villanyszerelési projektjét, és garantáljuk az eredményes és zökkenőmentes elektromos működést hosszú távon.',
+            'button_content' => 'időpontkérés',
+        ],
+    ],
+    'contact_form' => [
+        'title' => 'KAPCSOLATFELVÉTEL',
+        'subtitle' => 'Írja meg megrendelését az üzenőmezőbe',
+        'name' => [
+            'placeholder' => 'Név',
+        ],
+        'email' => [
+            'placeholder' => 'Email',
+        ],
+        'phone' => [
+            'placeholder' => 'Telefon',
+        ],
+        'message' => [
+            'placeholder' => 'Üzenet',
+        ],
+        'button_title' => 'ÜZENET KÜLDÉSE',
+    ],
+];
