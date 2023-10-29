e048581605f9d36d25fac66d3e8bcf8d7130de54
ezegyfa2 <darvasvok@gmail.com>
2023-10-29 07:29:56 +0200

    Prod

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require('laravel-mix')
 const path = require('path')
+require('helper-vue-components/AssetCompiler.js')
 const skewContainerImageGenerator = require('./node_modules/electrician-vue-components/src/Welcome/SkewCarousel/SkewContainer/Images/generator.js')
 
 global.currentWebpackConfig = {
@@ -24,8 +25,9 @@ global.currentWebpackConfig = {
 }
 mix.webpackConfig(currentWebpackConfig)
 
-skewContainerImageGenerator.generate()
-/*mix.js('resources/js/basicPackages.js', 'public/js').vue()
+//compileAssets(mix, 'electrician-vue-components', [ 'Welcome' ])
+//skewContainerImageGenerator.generate()
+mix.js('resources/js/basicPackages.js', 'public/js').vue()
 mix.js('resources/js/welcome.js', 'public/js').vue()
-mix.sass('resources/sass/welcome.scss', 'public/css/')*/
+mix.sass('resources/sass/welcome.scss', 'public/css/')
 //mix.copy('node_modules/electrician-vue-components/src/Welcome/CompiledTemplate.json', 'app/Templates/Welcome.json')
